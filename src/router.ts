0cc664646defb0f78368a2386321cd7dc16af1ee
Walter Alcantara <waltermalcantara@gmail.com>
2022-11-15 13:37:03 -0300

    built orders route

--- src/router.ts
+++ src/router.ts
@@ -7,6 +7,10 @@ import { listCategories } from './app/useCases/categories/listCategories';
 import { createProduct } from './app/useCases/products/createProduct';
 import { listProducts } from './app/useCases/products/listProducts';
 import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory';
+import { listOrders } from './app/useCases/orders/listOrders';
+import { createOrder } from './app/useCases/orders/createOrder';
+import { changeOrderStatus } from './app/useCases/orders/changeOrderStatus';
+import { cancelOrder } from './app/useCases/orders/cancelOrder';
 
 export const router = Router();
 
@@ -31,14 +35,10 @@ router.post('/products', upload.single('image'), createProduct);
 
 router.get('/categories/:categoryId/products', listProductsByCategory);
 
-router.post('/orders', (req, res) => {
-  res.send('OK');
-});
+router.get('/orders', listOrders);
 
-router.patch('/orders/:orderId', (req, res) => {
-  res.send('OK');
-});
+router.post('/orders', createOrder);
 
-router.delete('/orders/:orderId', (req, res) => {
-  res.send('OK');
-});
+router.patch('/orders/:orderId', changeOrderStatus);
+
+router.delete('/orders/:orderId', cancelOrder);
