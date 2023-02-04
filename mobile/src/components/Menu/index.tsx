7123bfd96fa303a8c54417b51ddf610b97e95976
Walter Alcantara <waltermalcantara@gmail.com>
2023-02-04 10:23:50 -0300

    menu component done

--- mobile/src/components/Menu/index.tsx
+++ mobile/src/components/Menu/index.tsx
@@ -1,3 +1,43 @@
+import { FlatList, TouchableOpacity } from 'react-native';
+
+import { products } from '../../mocks/products';
+import { formatCurrency } from '../../utils/formatCurrency';
+import { PlusCircle } from '../Icons/PlusCircle';
+
+import { Text } from '../Text';
+
+import * as S from './styled';
+
 export function Menu() {
-  return null;
+  return (
+    <FlatList
+      data={products}
+      style={{ marginTop: 32 }}
+      contentContainerStyle={{ paddingHorizontal: 24 }}
+      keyExtractor={(product) => product._id}
+      ItemSeparatorComponent={S.Separator}
+      renderItem={({ item: product }) => (
+        <S.Product>
+          <S.ProductImage
+            source={{
+              uri: `http://192.168.0.129:3001/uploads/${product.imagePath}`,
+            }}
+          />
+          <S.ProductDetails>
+            <Text weight="600">{product.name}</Text>
+            <Text size={14} color="#666" style={{ marginVertical: 8 }}>
+              {product.description}
+            </Text>
+            <Text size={14} weight="600">
+              {formatCurrency(product.price)}
+            </Text>
+          </S.ProductDetails>
+
+          <S.AddToCartButton>
+            <PlusCircle />
+          </S.AddToCartButton>
+        </S.Product>
+      )}
+    />
+  );
 }
